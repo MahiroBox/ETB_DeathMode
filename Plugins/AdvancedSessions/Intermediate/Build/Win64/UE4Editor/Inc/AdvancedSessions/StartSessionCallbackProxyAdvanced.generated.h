c35a4baf142091e1dd799638d720eeb01a4f2a55
Playeroth <50618172+Playeroth@users.noreply.github.com>
2025-04-15 18:04:19 -0300

    d

---
+++ Plugins/AdvancedSessions/Intermediate/Build/Win64/UE4Editor/Inc/AdvancedSessions/StartSessionCallbackProxyAdvanced.generated.h
@@ -0,0 +1,105 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+/*===========================================================================
+	Generated code exported from UnrealHeaderTool.
+	DO NOT modify this manually! Edit the corresponding .h files instead!
+===========================================================================*/
+
+#include "UObject/ObjectMacros.h"
+#include "UObject/ScriptMacros.h"
+
+PRAGMA_DISABLE_DEPRECATION_WARNINGS
+class UObject;
+class UStartSessionCallbackProxyAdvanced;
+#ifdef ADVANCEDSESSIONS_StartSessionCallbackProxyAdvanced_generated_h
+#error "StartSessionCallbackProxyAdvanced.generated.h already included, missing '#pragma once' in StartSessionCallbackProxyAdvanced.h"
+#endif
+#define ADVANCEDSESSIONS_StartSessionCallbackProxyAdvanced_generated_h
+
+#define MainMod_Plugins_AdvancedSessions_Source_AdvancedSessions_Classes_StartSessionCallbackProxyAdvanced_h_10_SPARSE_DATA
+#define MainMod_Plugins_AdvancedSessions_Source_AdvancedSessions_Classes_StartSessionCallbackProxyAdvanced_h_10_RPC_WRAPPERS \
+ \
+	DECLARE_FUNCTION(execStartAdvancedSession);
+
+
+#define MainMod_Plugins_AdvancedSessions_Source_AdvancedSessions_Classes_StartSessionCallbackProxyAdvanced_h_10_RPC_WRAPPERS_NO_PURE_DECLS \
+ \
+	DECLARE_FUNCTION(execStartAdvancedSession);
+
+
+#define MainMod_Plugins_AdvancedSessions_Source_AdvancedSessions_Classes_StartSessionCallbackProxyAdvanced_h_10_INCLASS_NO_PURE_DECLS \
+private: \
+	static void StaticRegisterNativesUStartSessionCallbackProxyAdvanced(); \
+	friend struct Z_Construct_UClass_UStartSessionCallbackProxyAdvanced_Statics; \
+public: \
+	DECLARE_CLASS(UStartSessionCallbackProxyAdvanced, UOnlineBlueprintCallProxyBase, COMPILED_IN_FLAGS(0), CASTCLASS_None, TEXT("/Script/AdvancedSessions"), ADVANCEDSESSIONS_API) \
+	DECLARE_SERIALIZER(UStartSessionCallbackProxyAdvanced)
+
+
+#define MainMod_Plugins_AdvancedSessions_Source_AdvancedSessions_Classes_StartSessionCallbackProxyAdvanced_h_10_INCLASS \
+private: \
+	static void StaticRegisterNativesUStartSessionCallbackProxyAdvanced(); \
+	friend struct Z_Construct_UClass_UStartSessionCallbackProxyAdvanced_Statics; \
+public: \
+	DECLARE_CLASS(UStartSessionCallbackProxyAdvanced, UOnlineBlueprintCallProxyBase, COMPILED_IN_FLAGS(0), CASTCLASS_None, TEXT("/Script/AdvancedSessions"), ADVANCEDSESSIONS_API) \
+	DECLARE_SERIALIZER(UStartSessionCallbackProxyAdvanced)
+
+
+#define MainMod_Plugins_AdvancedSessions_Source_AdvancedSessions_Classes_StartSessionCallbackProxyAdvanced_h_10_STANDARD_CONSTRUCTORS \
+	/** Standard constructor, called after all reflected properties have been initialized */ \
+	ADVANCEDSESSIONS_API UStartSessionCallbackProxyAdvanced(const FObjectInitializer& ObjectInitializer = FObjectInitializer::Get()); \
+	DEFINE_DEFAULT_OBJECT_INITIALIZER_CONSTRUCTOR_CALL(UStartSessionCallbackProxyAdvanced) \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(ADVANCEDSESSIONS_API, UStartSessionCallbackProxyAdvanced); \
+	DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(UStartSessionCallbackProxyAdvanced); \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	ADVANCEDSESSIONS_API UStartSessionCallbackProxyAdvanced(UStartSessionCallbackProxyAdvanced&&); \
+	ADVANCEDSESSIONS_API UStartSessionCallbackProxyAdvanced(const UStartSessionCallbackProxyAdvanced&); \
+public:
+
+
+#define MainMod_Plugins_AdvancedSessions_Source_AdvancedSessions_Classes_StartSessionCallbackProxyAdvanced_h_10_ENHANCED_CONSTRUCTORS \
+	/** Standard constructor, called after all reflected properties have been initialized */ \
+	ADVANCEDSESSIONS_API UStartSessionCallbackProxyAdvanced(const FObjectInitializer& ObjectInitializer = FObjectInitializer::Get()) : Super(ObjectInitializer) { }; \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	ADVANCEDSESSIONS_API UStartSessionCallbackProxyAdvanced(UStartSessionCallbackProxyAdvanced&&); \
+	ADVANCEDSESSIONS_API UStartSessionCallbackProxyAdvanced(const UStartSessionCallbackProxyAdvanced&); \
+public: \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(ADVANCEDSESSIONS_API, UStartSessionCallbackProxyAdvanced); \
+	DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(UStartSessionCallbackProxyAdvanced); \
+	DEFINE_DEFAULT_OBJECT_INITIALIZER_CONSTRUCTOR_CALL(UStartSessionCallbackProxyAdvanced)
+
+
+#define MainMod_Plugins_AdvancedSessions_Source_AdvancedSessions_Classes_StartSessionCallbackProxyAdvanced_h_10_PRIVATE_PROPERTY_OFFSET
+#define MainMod_Plugins_AdvancedSessions_Source_AdvancedSessions_Classes_StartSessionCallbackProxyAdvanced_h_7_PROLOG
+#define MainMod_Plugins_AdvancedSessions_Source_AdvancedSessions_Classes_StartSessionCallbackProxyAdvanced_h_10_GENERATED_BODY_LEGACY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	MainMod_Plugins_AdvancedSessions_Source_AdvancedSessions_Classes_StartSessionCallbackProxyAdvanced_h_10_PRIVATE_PROPERTY_OFFSET \
+	MainMod_Plugins_AdvancedSessions_Source_AdvancedSessions_Classes_StartSessionCallbackProxyAdvanced_h_10_SPARSE_DATA \
+	MainMod_Plugins_AdvancedSessions_Source_AdvancedSessions_Classes_StartSessionCallbackProxyAdvanced_h_10_RPC_WRAPPERS \
+	MainMod_Plugins_AdvancedSessions_Source_AdvancedSessions_Classes_StartSessionCallbackProxyAdvanced_h_10_INCLASS \
+	MainMod_Plugins_AdvancedSessions_Source_AdvancedSessions_Classes_StartSessionCallbackProxyAdvanced_h_10_STANDARD_CONSTRUCTORS \
+public: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+#define MainMod_Plugins_AdvancedSessions_Source_AdvancedSessions_Classes_StartSessionCallbackProxyAdvanced_h_10_GENERATED_BODY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	MainMod_Plugins_AdvancedSessions_Source_AdvancedSessions_Classes_StartSessionCallbackProxyAdvanced_h_10_PRIVATE_PROPERTY_OFFSET \
+	MainMod_Plugins_AdvancedSessions_Source_AdvancedSessions_Classes_StartSessionCallbackProxyAdvanced_h_10_SPARSE_DATA \
+	MainMod_Plugins_AdvancedSessions_Source_AdvancedSessions_Classes_StartSessionCallbackProxyAdvanced_h_10_RPC_WRAPPERS_NO_PURE_DECLS \
+	MainMod_Plugins_AdvancedSessions_Source_AdvancedSessions_Classes_StartSessionCallbackProxyAdvanced_h_10_INCLASS_NO_PURE_DECLS \
+	MainMod_Plugins_AdvancedSessions_Source_AdvancedSessions_Classes_StartSessionCallbackProxyAdvanced_h_10_ENHANCED_CONSTRUCTORS \
+static_assert(false, "Unknown access specifier for GENERATED_BODY() macro in class StartSessionCallbackProxyAdvanced."); \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+template<> ADVANCEDSESSIONS_API UClass* StaticClass<class UStartSessionCallbackProxyAdvanced>();
+
+#undef CURRENT_FILE_ID
+#define CURRENT_FILE_ID MainMod_Plugins_AdvancedSessions_Source_AdvancedSessions_Classes_StartSessionCallbackProxyAdvanced_h
+
+
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
